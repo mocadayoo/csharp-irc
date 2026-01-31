7bfac37fb5f7c380a5f097c14132407b963e68cc
mocadayoo <192976698+mocadayoo@users.noreply.github.com>
2026-01-31 14:47:56 +0900

    add: decode message

--- IRCClient/Program.cs
+++ IRCClient/Program.cs
@@ -1,13 +1,25 @@
-﻿using System.Net.WebSockets;
+﻿using System.Collections.Specialized;
+using System.Data.SqlTypes;
+using System.Linq.Expressions;
+using System.Net.WebSockets;
+using System.Text;
+
+Uri ServerUri = new("ws://localhost:8080");
+ClientWebSocket Client = new ClientWebSocket();
+
+async Task SendAsync(ClientWebSocket client, string message)
+{
+    byte[] sendBuf = Encoding.UTF8.GetBytes(message);
+    await client.SendAsync(new ArraySegment<byte>(sendBuf), WebSocketMessageType.Text, true, CancellationToken.None);
+}
 
 try
 {
-    Uri ServerUri = new("ws://localhost:8080");
-    ClientWebSocket Client = new ClientWebSocket();
     async Task Main()
     {
         await Client.ConnectAsync(ServerUri, CancellationToken.None);
-        var buffer = new byte[1024];
+        
+        await SendAsync(Client, (string)"こんにちは");
     }
 
     await Main();
--- IRCServer/Program.cs
+++ IRCServer/Program.cs
@@ -6,7 +6,7 @@ using System.Security.Cryptography;
 
 void Main()
 {
-    Console.WriteLine("startup..");
+    Console.WriteLine("[info] startup..");
 
     TcpListener wsApp = new TcpListener(IPAddress.Parse("127.0.0.1"), 8080); // localの8080ポートで作成
 
@@ -14,7 +14,7 @@ void Main()
     while (true)
     {
         TcpClient client = wsApp.AcceptTcpClient();
-        Console.WriteLine("client connected");
+        Console.WriteLine("[info] client connected");
 
         Task.Run(() => HandlerClient(client));
     }
@@ -31,7 +31,7 @@ void HandlerClient(TcpClient client)
 
     if (Regex.IsMatch(s, "^GET", RegexOptions.IgnoreCase))
     {
-        Console.WriteLine("HandShake recive");
+        Console.WriteLine("[info] HandShake recive");
         // clientがランダムなキーをSec-WebSocket-Keyとしてheaderに入れてくる
         // それをGUID (RFC6455) で決められている "258EAFA5-E914-47DA-95CA-C5AB0DC85B11" とくっつける
         // それをsha-1でhashそしてbase64してからSec-WebSocket-Acceptに乗せて返す
@@ -50,6 +50,58 @@ void HandlerClient(TcpClient client)
         );
 
         stream.Write(response, 0, response.Length);
+    }
+
+    /*
+        bufferなので出す順番を間違えると崩壊する (当たり前)
+        長さによって 2^n のbyteにbufを広げないといけない
+        その長さはpayloadのlengthがheaderにあるので読む
+    */
+    while (true)
+    {
+        // [0]にはFINフラグ、種別が入っている
+        // [1] にはmaskのflagとdataのlength
+        byte[] head = new byte[2];
+        stream.ReadExactly(head);
+
+        bool isMask = (head[1] & 0x80) != 0; // 一番左の1bitを読む
+        long payloadLength = head[1] & 0x7F; // mask flag以外の右側の7bitを読む
+
+        byte[] lenBuf;
+        switch (payloadLength)
+        {
+            // length <= 125 まではそのまま使える
+            // それ以外のケースの場合2byte先,8byte先を読む
+            case 126:
+                lenBuf = new byte[2];
+                stream.ReadExactly(lenBuf);
+                Array.Reverse(lenBuf);
+                payloadLength = BitConverter.ToInt16(lenBuf, 0);
+                break;
+            case 127:
+                lenBuf = new byte[8];
+                stream.ReadExactly(lenBuf);
+                Array.Reverse(lenBuf);
+                payloadLength = BitConverter.ToInt64(lenBuf, 0);
+                break;
+        }
+
+        byte[] masks = new byte[4];
+        if (isMask) stream.ReadExactly(masks);
+
+        byte[] payload = new byte[payloadLength];
+        stream.ReadExactly(payload);
+
+        if (isMask)
+        {
+            for (int i = 0; i < payload.Length; i++)
+            {
+                payload[i] = (byte)(payload[i] ^ masks[i % 4]);
+            }
+        }
+
+        string message = Encoding.UTF8.GetString(payload);
+        Console.WriteLine($"[info] recive: {message}");
     }
 }
 
